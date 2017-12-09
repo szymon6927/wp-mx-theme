7ea67d8d8a288a7b668c2fff829a58db5acb3026
Szymon Miks <miks.szymon@gmail.com>
2017-12-09 22:32:07 +0100

    zmiany w naglowku szablonu

--- header.php
+++ header.php
@@ -34,11 +34,25 @@
     <div class="blog-masthead">
         <div class="container">
             <nav class="blog-nav">
-                <a class="blog-nav-item active" href="#">Home</a>
-                <a class="blog-nav-item" href="#">New features</a>
-                <a class="blog-nav-item" href="#">Press</a>
-                <a class="blog-nav-item" href="#">New hires</a>
-                <a class="blog-nav-item" href="#">About</a>
+                <?php
+                    wp_nav_menu( array(
+                        'theme_location'    => 'primary',
+                        'depth'             => 2,
+                        'container'         => 'div',
+                        'container_class'   => 'collapse navbar-collapse',
+                        'container_id'      => 'bs-example-navbar-collapse-1',
+                        'menu_class'        => 'nav navbar-nav',
+                        'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                        'walker'            => new WP_Bootstrap_Navwalker(),
+                    ) );
+                ?>
             </nav>
         </div>
-    </div>
+    </div> 
+
+    <div class="container">
+
+        <div class="blog-header">
+            <h1 class="blog-title"><?php bloginfo('name'); ?></h1>
+            <p class="lead blog-description"><?php bloginfo('description'); ?> </p>
+        </div>
