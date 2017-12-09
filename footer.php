54eb6f63a31054525e2e90e0d17c5cfecaf9e0eb
Szymon Miks <miks.szymon@gmail.com>
2017-12-09 22:32:51 +0100

    dodanie osobnego pliku z footerem

---
+++ footer.php
@@ -0,0 +1,18 @@
+    <footer class="blog-footer">
+        <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name') ?><p>
+            <a href="#">Back to top</a>
+        </p>
+    </footer>
+
+    <?php wp_footer(); ?>
+ 
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="<?php bloginfo('template_url') ?>/js/bootstrap.min.js"></script>
+</body>
+
+</html>
