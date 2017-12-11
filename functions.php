590bbab97515d2fe842f1c4c1d4a1ce435f7cde8
Szymon Miks <miks.szymon@gmail.com>
2017-12-11 21:53:49 +0100

    inicjalizacja widgetow

--- functions.php
+++ functions.php
@@ -10,6 +10,8 @@
             'primary' => __('Primary Menu')
         ));
 
+        // Post Formats
+        add_theme_support('post-formats', array('aside', 'gallery'));
     }
 
     add_action('after_setup_theme', 'wpb_theme_setup');
@@ -19,5 +21,19 @@
         return 25;
     }
 
-    add_filter('excerpt_length', 'set_exception_length')
+    add_filter('excerpt_length', 'set_exception_length');
+
+    // Widget locations
+    function wpb_init_widgets($id) {
+        register_sidebar(array(
+            'name'  => 'Sidebar',
+            'id'    => 'sidebar',
+            'before_widget' => '<div class="sidebar-module">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h4>',
+            'after_title'   => '</h4>'
+        ));
+    };
+
+    add_action('widgets_init', 'wpb_init_widgets');
 ?>
