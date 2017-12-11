0dc91b276c1fbddf27637d7f180d8eace3cef17e
Szymon Miks <miks.szymon@gmail.com>
2017-12-11 21:53:24 +0100

    przeniesienie do footera sidebar'a

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+    <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+                <?php if(is_active_sidebar('sidebar')) : ?>
+                    <?php dynamic_sidebar('sidebar'); ?>
+                <?php endif; ?>
+            </div>
+            <!-- /.blog-sidebar -->
+
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+    
     <footer class="blog-footer">
         <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name') ?><p>
             <a href="#">Back to top</a>
