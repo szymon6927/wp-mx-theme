ea6f1ff9863092a7cf3e071332930931deaa09a6
Szymon Miks <miks.szymon@gmail.com>
2017-12-12 23:34:20 +0100

    dodanie do footera particle.js oraz craetive.min.js oraz moj app.js

--- footer.php
+++ footer.php
@@ -1,16 +1,18 @@
-    <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+        <?php if(!is_front_page()) : ?>
+            <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
                 <?php if(is_active_sidebar('sidebar')) : ?>
                     <?php dynamic_sidebar('sidebar'); ?>
                 <?php endif; ?>
             </div>
             <!-- /.blog-sidebar -->
 
+        <?php endif; ?>
         </div>
         <!-- /.row -->
 
     </div>
     <!-- /.container -->
-    
+    </div>
     <footer class="blog-footer">
         <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name') ?><p>
             <a href="#">Back to top</a>
@@ -26,6 +28,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="<?php bloginfo('template_url') ?>/js/bootstrap.min.js"></script>
+    <script src="<?php bloginfo('template_url') ?>/js/particles.min.js"></script>
+    <!-- <script src="<?php bloginfo('template_url') ?>/js/creative.min.js"></script> -->
+    <script src="<?php bloginfo('template_url') ?>/js/app.js"></script>
 </body>
 
 </html>
