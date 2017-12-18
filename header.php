107ccb4ec01ebf67f7fdc18bb8d7ae7824c98bb4
Szymon Miks <miks.szymon@gmail.com>
2017-12-18 21:26:44 +0100

    obrazek jako tło dla sekcji osiągnięć

--- header.php
+++ header.php
@@ -24,18 +24,13 @@
             .particles-js-canvas-el {
                 background: #333 url(<?php echo get_theme_mod('showcase_image', get_bloginfo('template_url').'/img/showcase.jpg'); ?>); ?>) no-repeat center
             }
+            .achievements {
+                background: #008149 url(<?php echo get_bloginfo('template_url').'/img/analysis2.jpg'; ?>)
+            }
             </style>
     <?php endif; ?>
-    <!-- Custom styles for this template -->
-    <!-- <link href="<?php bloginfo('template_url'); ?>/css/creative.min.css" rel="stylesheet"> -->
+    
     <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
     <?php wp_head(); ?>
 </head>
 
