e96660d2ef3e0a577bc38b473ddb06851f354c34
Szymon Miks <miks.szymon@gmail.com>
2017-12-18 21:26:03 +0100

    dodanie patronów oraz osiągnięć

--- front-page.php
+++ front-page.php
@@ -24,7 +24,7 @@
             <p class="text-faded mb-4">
             Celem prac Studenckiego Koła Naukowego Genetyków Zwierząt jest poznanie sekwencji genów kandydatów odpowiadających za proces melanogenzey 
             oraz określenie wpływu mutacji/polimorfizmów w nich obserwowanych na modelu norki amerykańskiej Neovison vison.</p>
-            <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
+            <a class="btn btn-light btn-xl js-scroll-trigger custom-btn" href="#services">Czytaj więcej!</a>
           </div>
         </div>
       </div>
@@ -34,7 +34,7 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-12 text-center">
-            <h2 class="section-heading">W chwili obecnej trwają prace na genami</h2>
+            <h2 class="section-heading">W chwili obecnej trwają prace nad genami:</h2>
             <hr class="my-4">
           </div>
         </div>
@@ -97,31 +97,78 @@
 
     <section class="bg-dark box-shadow pad80 text-white achievements">
       <div class="container text-center">
-        <h2 class="mb-4">Zobacz nasze osiagniecia</h2>
+        <h2 class="mb-4">Nasze osiągnięcia</h2>
         <hr class="contrast">
-        <a class="btn btn-light custom-btn btn-xl" href="#">Czytaj więcej!</a>
+        <div class="col-sm-4 col-xs-6">
+          <div class="achievements-item">
+            <div class="achievements-title text-center">
+              <i class="fa fa-trophy" aria-hidden="true"></i>
+            </div>
+            <div class="achievements-desc">
+              <p>Zdeponowanie w National Center of Biotechnology Information (NCBI) pionierskich sekwencji genów EDNRB, 
+                PAX3 oraz KIT norki amerykańskiej (Neovison vison).</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4 col-xs-6">
+          <div class="achievements-item">
+            <div class="achievements-title text-center">
+              <i class="fa fa-trophy" aria-hidden="true"></i>
+            </div>
+            <div class="achievements-desc">
+              <p>Wystawienie referatu pt. "Rola genu KIT w procesie melanogenezy" podczas studenckiej sesji kół naukowych na ZUT w Szczecinie.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4 col-xs-6">
+          <div class="achievements-item">
+            <div class="achievements-title text-center">
+              <i class="fa fa-trophy" aria-hidden="true"></i>
+            </div>
+            <div class="achievements-desc">
+              <p>Wystawienie referatu pt. "Rola genu PAX3 w procesie melanogenezy" podczas studenckiej sesji kół naukowych na ZUT w Szczecinie.</p>
+            </div>
+          </div>
+        </div>
+        <a class="btn btn-light custom-btn btn-xl mt30" href="#">Zobacz więcej!</a>
       </div>
     </section>
 
-    <section id="contact">
+    <section id="patronate" class="pad80">
       <div class="container">
         <div class="row">
-          <div class="col-lg-8 mx-auto text-center">
+          <h2>Wspomagają nas</h2>
+          <hr>
+          <div class="col-sm-4 col-sm-offset-4">
+            <a href="https://pl.promega.com/">
+              <img src="https://wwwen.uni.lu/var/storage/images/media/images/promega_logo_sol/1146786-1-fre-FR/promega_logo_sol.jpg" class="img-responsive patronate-logo" alt="Promega logo">
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="contact" class="pad80">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-lg-offset-2 text-center">
             <h2 class="section-heading">Zainteresowany?</h2>
             <hr class="my-4">
             <p class="mb-5">Zostaw nam kontakt do Siebie</p>
           </div>
         </div>
         <div class="row">
-          <div class="col-lg-4 ml-auto text-center">
-            <i class="fa fa-phone fa-3x mb-3 sr-contact"></i>
-            <p>123-456-6789</p>
-          </div>
-          <div class="col-lg-4 mr-auto text-center">
-            <i class="fa fa-envelope-o fa-3x mb-3 sr-contact"></i>
-            <p>
-              <a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a>
-            </p>
+          <div class="col-lg-8 col-lg-offset-2 text-center mt20">
+            <div class="col-lg-6 text-center">
+              <i class="fa fa-phone fa-3x mb-3 sr-contact"></i>
+              <p>+(48) 123-456-6789</p>
+            </div>
+            <div class="col-lg-6 text-center">
+              <i class="fa fa-envelope-o fa-3x mb-3 sr-contact"></i>
+              <p>
+                <a href="mailto:miks.szymon@gmail.com">miks.szymon@gmail.com</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
