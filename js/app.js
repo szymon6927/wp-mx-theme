3ae172a60298b624fde8e430e4c4fe63c60af397
Szymon Miks <miks.szymon@gmail.com>
2017-12-19 22:57:29 +0100

    dodanie js'a na sticky footer

--- js/app.js
+++ js/app.js
@@ -28,3 +28,37 @@ $(document).ready(function() {
   });
  });
 })
+
+$(window).bind("load", function() { 
+     
+ let footerHeight = 0,
+     footerTop = 0,
+     $footer = $(".blog-footer");
+     
+ positionFooter();
+ 
+ function positionFooter() {
+  footerHeight = $footer.height();
+  // 20 is as padding height
+  footerTop = ($(window).scrollTop()+$(window).height()-footerHeight-61)+"px";
+
+  if ( ($(document.body).height()+footerHeight) < $(window).height()) {
+     $footer.css({
+          position: "absolute",
+          width: "100%"
+     }).animate({
+          top: footerTop
+     })
+  } 
+  else {
+    $footer.css({
+      position: "static"
+    })
+  }
+
+}
+
+$(window)
+  .scroll(positionFooter)
+  .resize(positionFooter)
+});
