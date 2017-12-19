7d290ea7411165be5fbe899d4f752cdf8524a22a
Szymon Miks <miks.szymon@gmail.com>
2017-12-19 22:58:06 +0100

    zmiana layaoutu dla pojedynczej strony wpisu blogowego

--- page.php
+++ page.php
@@ -1,23 +1,16 @@
 <?php get_header(); ?>
 
-        <div class="row">
-
-            <div class="col-sm-8 blog-main">
-
-                <?php if(have_posts()) : ?>
-                    <?php while(have_posts()) : the_post(); ?>
-                    <div class="blog-post">
-                        <h2 class="blog-post-title">
-                            <?php the_title(); ?>
-                        </h2>
-                        <?php the_content(); ?>
-                    </div>
-                    <?php endwhile; ?>
-                <?php else : ?>
-                    <p><?php __('No Page Found'); ?></p>
-                <?php endif; ?>
-
+    <div class="col-lg-12 col-md-12 col-sm12 col-xs-12">
+        <h1 class="text-center"><?php single_post_title(); ?></h1>
+        <?php if(have_posts()) : ?>
+            <?php while(have_posts()) : the_post(); ?>
+            <div class="content text-left mt10">
+                <?php the_content(); ?>
             </div>
-            <!-- /.blog-main -->
+            <?php endwhile; ?>
+        <?php else : ?>
+            <p><?php __('No Page Found'); ?></p>
+        <?php endif; ?>
+    </div>
 
     <?php get_footer(); ?>
