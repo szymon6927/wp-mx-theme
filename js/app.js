bdee002828a540781a3629ac59e0ff9fa3f59f5d
Szymon Miks <miks.szymon@gmail.com>
2017-12-23 00:27:25 +0100

    zmiany w skrypcie od sticky footer ponieważ po dodaniu loga zmieniła się wysokość footera

--- js/app.js
+++ js/app.js
@@ -40,7 +40,7 @@ $(window).bind("load", function() {
  function positionFooter() {
   footerHeight = $footer.height();
   // 20 is as padding height
-  footerTop = ($(window).scrollTop()+$(window).height()-footerHeight-61)+"px";
+  footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
 
   if ( ($(document.body).height()+footerHeight) < $(window).height()) {
      $footer.css({
@@ -61,4 +61,40 @@ $(window).bind("load", function() {
 $(window)
   .scroll(positionFooter)
   .resize(positionFooter)
+});
+
+
+$('.responsive').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
