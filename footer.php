786e499bfd4ed049252d0a8c342add1545044fad
Szymon Miks <miks.szymon@gmail.com>
2017-12-23 00:24:45 +0100

    pozbycie sie sidebar z footer.php, dodanie do footera loga szkola i loga wydziału, dołączanie pliku js od slick slidera

--- footer.php
+++ footer.php
@@ -1,26 +1,37 @@
-        <?php if(!is_front_page()) : ?>
-            <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
-                <?php if(is_active_sidebar('sidebar')) : ?>
-                    <?php dynamic_sidebar('sidebar'); ?>
-                <?php endif; ?>
-            </div>
-            <!-- /.blog-sidebar -->
 
-        <?php endif; ?>
         </div>
         <!-- /.row -->
-
     </div>
     <!-- /.container -->
     </div>
     <p id="back-top" style="display: block;"><a title="Przejdź do góry strony" class="defaultCursor">Przejdź do góry strony</a></p>
     <footer class="blog-footer">
-        <div class="container">
-            <div class="col-sm-6 col-xs-12 text-left">
-                <span>Copyright © <?php echo Date('Y'); ?> <strong><?php bloginfo('name') ?></strong> All Rights Reserved</span>
+        <div class="container-fluid top-footer">
+            <div class="container">
+                <div class="col-md-6 col-sm-6 col-xs-12 text-center">
+                    <a href="http://www.biotechnologia.zut.edu.pl/" target="_blank" rel="nofollow">
+                        <div class="img-responsive wbihz">
+                            <img class="patronate-logo" src="<?php bloginfo('template_url'); ?>/img/wbihz-png.png" />
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-12 text-center">
+                    <a href="http://www.zut.edu.pl/" target="_blank" rel="nofollow">
+                        <div class="img-responsive zut">
+                            <img class="patronate-logo" src="<?php bloginfo('template_url'); ?>/img/zut-png.png" />
+                        </div>
+                    </a>
+                </div>
             </div>
-            <div class="col-sm-6 col-xs-12 text-right">
-                <p class="created">Created by <a href="mailto:miks.szymon@gmail.com">Szymon Miks</a></p>
+        </div>
+        <div class="container-fluid bottom-footer">
+            <div class="container">
+                <div class="col-sm-6 col-xs-12 text-left">
+                    <span>Copyright © <?php echo Date('Y'); ?> <strong><?php bloginfo('name') ?></strong> All Rights Reserved</span>
+                </div>
+                <div class="col-sm-6 col-xs-12 text-right">
+                    <p class="created">Created by <a href="mailto:miks.szymon@gmail.com">Szymon Miks</a></p>
+                </div>
             </div>
         </div>
     </footer>
@@ -31,6 +42,7 @@
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="<?php bloginfo('template_url') ?>/js/bootstrap.min.js"></script>
     <script src="<?php bloginfo('template_url') ?>/js/particles.min.js"></script>
+    <script src="<?php bloginfo('template_url') ?>/js/slick.min.js"></script>
     <script src="<?php bloginfo('template_url') ?>/js/app.js"></script>
 </body>
 
