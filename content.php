1b1f6a693ef469f1ad37286962ae801017b9edb8
Szymon Miks <miks.szymon@gmail.com>
2017-12-23 00:23:02 +0100

    minimalizacja template content.php do minimum, wyświetlanie samego tekstu plus thumbnail jeżeli istnieje

--- content.php
+++ content.php
@@ -1,20 +1,6 @@
 <div class="blog-post">
-    <?php if(is_single()) : ?>
-        <h2 class="blog-post-title"><?php the_title(); ?></h2>
-    <?php else: ?>
-        <a href="<?php the_permalink() ?>">
-            <h2 class="blog-post-title"><?php the_title(); ?></h2>
-        </a>
-    <?php endif; ?>
-    <p class="blog-post-meta">
-        <?php the_time('F j Y,  g:i a'); ?>
-        by
-        <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
-            <?php the_author(); ?>
-        </a>
-    </p>
     <?php if(has_post_thumbnail()) : ?>
-        <div class="post-thumb">
+        <div class="post-thumb thumb-in-content">
             <?php the_post_thumbnail(); ?>
         </div>
     <?php endif; ?>
@@ -23,9 +9,5 @@
         <?php the_content(); ?>
     <?php else: ?>
         <?php the_excerpt(); ?>
-    <?php endif; ?>
-
-    <?php if(is_single()) : ?>
-        <?php comments_template(); ?>
     <?php endif; ?>
 </div>
