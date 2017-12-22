2e43b700a22c6d91881671f728f6cf9a5749074a
Szymon Miks <miks.szymon@gmail.com>
2017-12-23 00:30:03 +0100

    wstepny projekt wygladu pojedynczego newsa

--- single.php
+++ single.php
@@ -1,18 +1,36 @@
 <?php get_header(); ?>
-
-        <div class="row">
-
-            <div class="col-sm-8 blog-main">
-                <h1>single.php</h1>
-                <?php if(have_posts()) : ?>
-                    <?php while(have_posts()) : the_post(); ?>
-                        <?php get_template_part('content', get_post_format()) ?>
-                    <?php endwhile; ?>
-                <?php else : ?>
-                    <p><?php __('No Posts Found'); ?></p>
-                <?php endif; ?>
-
+    <?php if(have_posts()) : ?>
+        <?php while(have_posts()) : the_post(); ?>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="masthead" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/img/analysis2.jpg')">
+                        <div class="overlay"></div>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 mx-auto">
+                                    <div class="post-heading">
+                                        <h1><?php single_post_title(); ?></h1>
+                                        <span class="meta">Opublikowano przez
+                                            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> - 
+                                            <?php the_time('d/m/Y, g:i'); ?>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <!-- /.blog-main -->
-
-    <?php get_footer(); ?>
+            
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8 col-sm-offset-2 blog-main">
+                        <?php get_template_part('content', get_post_format()) ?>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <p><?php __('No Posts Found'); ?></p>
+    <?php endif; ?>
+<?php get_footer(); ?>
