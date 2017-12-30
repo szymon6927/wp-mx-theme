d5113a95d85caef4f306c3b818f6dcbf3ed105c4
Szymon Miks <miks.szymon@gmail.com>
2017-12-30 19:44:32 +0100

    przechodzenie z animacja z jednej sekcji do drugiej na stronie głównej

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+// This is a functions that scrolls to #{blah}link
+function goToByScroll(id) {
+  // Remove "link" from the ID
+  id = id.replace("link", "");
+  // Scroll
+  $('html,body').animate({
+      scrollTop: $("#" + id).offset().top - 80
+    },
+    'slow');
+}
+
 particlesJS.load('particles-js', 'wp-content/themes/mx-theme/js/particles.json', function () {
   console.log('callback - particles.js config loaded');
 });
@@ -9,6 +20,16 @@ $(window).scroll(function () {
     $('nav').removeClass('shrink');
   }
 });
+
+$("#see-more").click(function(e) {
+  e.preventDefault();
+  goToByScroll("about");
+});
+
+$('#go-to-services').click(function(e) {
+  e.preventDefault();
+  goToByScroll("services");
+})
 
 $(document).ready(function() {
   $("#back-top").hide();
@@ -64,37 +85,34 @@ $(window)
 });
 
 
-$('.responsive').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
+// $('.responsive').slick({
+//   dots: true,
+//   infinite: false,
+//   speed: 300,
+//   slidesToShow: 4,
+//   slidesToScroll: 4,
+//   responsive: [{
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//   ]
+// });
